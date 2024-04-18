2edaa8ca1594d12599ff59005a20892e79e1fb62
Samsonov Alexey <alexasamsonov@gmail.com>
2024-04-18 16:30:06 +0300

    MainWindow

---
+++ Lab2/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+
+namespace Client
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonAddSword_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonAddAxe_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
